9a818cd810eab7f4537078e02a8f01ae5f2e4885
Matthew Pelmear <mjpelmear@gmail.com>
2021-09-11 16:22:13 -0700

    Helpful notes

--- startup.m
+++ startup.m
@@ -1,3 +1,5 @@
+% Help: https://www.mathworks.com/help/matlab/matlab_env/add-folders-to-matlab-search-path-at-startup.html
+
 % Need these folders in matlab's path so everything works properly
 addpath ./analysis
 addpath ./ui-components
